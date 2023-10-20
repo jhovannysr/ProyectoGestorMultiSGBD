70e2b560fcf7f274c247d1041a0eb2b3211bfd56
jhovannysr <84175470+jhovannysr@users.noreply.github.com>
2023-10-20 23:16:41 +0200

    Add files via upload

---
+++ EmpresaAD/src/main/java/view/MenuEmpleado.java
@@ -0,0 +1,22 @@
+package view;
+
+import dao.EmpleadoGestor;
+
+public class MenuEmpleado {
+
+	/**
+	 * Clase a gestionar
+	 */
+	private EmpleadoGestor empleGestor;
+
+	/**
+	 * Constructor
+	 * @param empleGestor
+	 */
+	public MenuEmpleado(EmpleadoGestor empleGestor) {
+		this.empleGestor = empleGestor;
+	}
+	
+	
+	
+}
