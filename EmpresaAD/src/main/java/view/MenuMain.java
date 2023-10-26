95d5dd47a3efffbf9162dac8cc1a3b221eb9bd9c
Jean Paul <124582378+JeanPElguera@users.noreply.github.com>
2023-10-26 13:24:48 +0200

    Add files via upload

---
+++ EmpresaAD/src/main/java/view/MenuMain.java
@@ -0,0 +1,52 @@
+package view;
+
+import java.util.List;
+
+import dao.BD;
+import dao.DepartmentDAO;
+import dao.EmployeeDAO;
+import io.IO;
+
+
+/**
+ * Gestor de departamentos y empleados
+ * 
+ * @since 23-10-2023
+ * Apellidos temporales, no se los reales
+ * @author Henry Jhovanny Aucapiña Tapia, Paul Elver galarga, Naim El Mostafa
+ */
+public class MenuMain {
+
+	public static void main(String[] args) {
+		List<String> opciones = List.of("MENU Departamento (D)"
+				, "MENU Empleado (E)", "Update (U)","Salir (S)");
+		
+		while (true) {
+			System.out.println(opciones);
+			switch (IO.readUpperChar()) {
+			case 'D':
+				var departGestor = new DepartmentDAO();
+				new MenuDepartment(departGestor); 
+				break;
+			case 'E':
+				var empleadoGestor = new EmployeeDAO();
+				new MenuEmployee(empleadoGestor);
+				break;
+			case 'S':
+				closeConnectionDB();
+				return;
+			case 'X':
+				BD.getConnection();
+				BD.deleteDB();;
+				BD.close();
+				System.err.println("Base de datos eliminada");
+				return;
+			default:
+			}
+		}
+	}
+
+	private static void closeConnectionDB() {
+		BD.close();
+	}
+}
