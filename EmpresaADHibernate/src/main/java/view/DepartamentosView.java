54adc7c296ab690c0d4310c1d0c3dc2dfe7a8b24
naimElmt <146950915+naimElmt@users.noreply.github.com>
2023-11-06 08:29:53 +0100

    Add files via upload

---
+++ EmpresaADHibernate/src/main/java/view/DepartamentosView.java
@@ -0,0 +1,78 @@
+package view;
+
+import java.util.List;
+import java.util.Optional;
+
+import io.IO;
+import model.Departamento;
+import model.Empleado;
+
+public class DepartamentosView {
+
+	final List<String> opciones = List.of("buscar por Código", "buscar por Nombre", "Mostrar", "Añadir", "modiFicar",
+			"Eliminar", "Salir");
+
+	public Character getOption() {
+		IO.println("Departamentos: " + opciones);
+		return Character.toUpperCase(IO.readChar());
+	}
+
+	public Departamento create() {
+		IO.print("Nombre ? ");
+		String nombre = IO.readString();
+		Departamento d = Departamento.builder().nombre(nombre).build();
+		return d;
+	}
+
+//	public Departamento modificar(Departamento d) {
+//		if (d == null) {
+//			IO.println("No se ha encontrado el departamento");
+//			return null;
+//		}
+//		IO.printf("Nombre [%s] ? ", d.getNombre());
+//		String nombre = IO.readString();
+//		if (!nombre.isBlank()) {
+//			d.setNombre(nombre);
+//		}
+//		IO.printf("Jefe [%s] ? ", d.getJefe() == null ? "sin jefe!!!" : d.getJefe().show());
+//		Integer jefe = IO.readIntOrNull();
+//		if (jefe != null) {
+//			d.setJefe(Empleado.builder().id(jefe).build());
+//		}
+//		return d;
+//	}
+//
+	public String buscarPorInicioDelNombre() {
+		IO.print("El nombre empieza por ? ");
+		return IO.readString();
+	}
+
+	public int buscarPorId() {
+		IO.print("Código ? ");
+		return IO.readInt();
+	}
+
+	public void mostrar(List<Departamento> list) {
+		list.forEach(System.out::println);
+	}
+	
+	public void mostrarPorId(Optional<Departamento> list) {
+		list.stream().forEach(System.out::println);
+	}
+//
+//	public void mostrar(Departamento d, List<Empleado> emps) {
+//		if (d == null) {
+//			return;
+//		}
+//		IO.println(d.show());
+//		IO.println("* Empleados del departamento :");
+//		for (Empleado e : emps) {
+//			IO.println(e.show());
+//		}
+//	}
+//
+	public void result(String msg) {
+		IO.println(msg);
+	}
+
+}
